d8ed4df10e314b476f253c5ad76df456e5366095
abhishekkank47 <128067957+abhishekkank47@users.noreply.github.com>
2024-09-02 17:33:24 +0530

    new url

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(
   cors({
-    origin : ['http://localhost:5173', 'https://your-frontend-domain.vercel.app'],
+    origin : ['https://pet-selling-system-ecommerce-platform-ritrieash.vercel.app'],
     methods : ['GET','POST','PUT','DELETE'],
     credentials : true
   }))
